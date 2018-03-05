11507aa7f36a032651e22906a62a3040e9f72d7a
Danceiny <danceiny@gmail.com>
2018-03-05 21:06:39 +0800

    [DEBUG] for test

--- dl/caffe/tests/run.sh
+++ dl/caffe/tests/run.sh
@@ -1,6 +1,6 @@
 
 
-# docker build --rm -t registry.cn-shanghai.aliyuncs.com/russellcloud/caffe:2-py2 -f Dockerfile-py2 .
+# docker build --rm -t registry.cn-shanghai.aliyuncs.com/russellcloud/caffe:2.0-py2.7 -f Dockerfile-py2 .
 
 cd ~ && python -c 'from caffe2.python import core' 2>/dev/null && echo "Success" || echo "Failure"
 
