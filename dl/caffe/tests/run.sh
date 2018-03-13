d3a650d86f1c1310dbde3f5d618ffc7eecd08998
Danceiny <danceiny@gmail.com>
2018-03-13 17:35:30 +0800

    update

--- dl/caffe/tests/run.sh
+++ dl/caffe/tests/run.sh
@@ -1,13 +1,13 @@
 
 
-# docker build --rm -t registry.cn-shanghai.aliyuncs.com/russellcloud/caffe:2.0-py2.7 -f Dockerfile-py2 .
+# docker build --rm -t registry-vpc.cn-shanghai.aliyuncs.com/russellcloud/caffe:2.0-py2.7 -f Dockerfile-py2 .
 
 cd ~ && python -c 'from caffe2.python import core' 2>/dev/null && echo "Success" || echo "Failure"
 
 
 
 # GPU test
-cd ~/caff2 && python caffe2/python/operator_test/relu_op_test.py
+cd caffe2 && python caffe2/python/operator_test/relu_op_test.py
 
 # ERROR LOGS
 #1019/caffe2/caffe2/operators/conv_op_eigen.cc:24:2: error: #error "Caffe2 requires Eigen to be at least 3.3.0.";
